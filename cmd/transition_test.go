a87908278ec69a8852df1d361a8a51c73ecba150
haijima <k.haijima@gmail.com>
2023-01-29 01:15:40 +0900

    test: Add test cases

--- cmd/transition_test.go
+++ cmd/transition_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 
@@ -13,10 +14,112 @@ import (
 func TestNewTransitionCmd(t *testing.T) {
 	p := stool.NewTransitionProfiler()
 	v := viper.GetViper()
-	fs := afero.NewOsFs()
+	fs := afero.NewMemMapFs()
 	cmd := NewTransitionCmd(p, v, fs)
 
 	assert.Equal(t, "transition", cmd.Name(), "NewTransitionCmd() should return command named \"transition\". but: \"%s\"", cmd.Name())
+}
+
+func TestNewTransitionCmd_Flag(t *testing.T) {
+	p := stool.NewTransitionProfiler()
+	v := viper.GetViper()
+	fs := afero.NewMemMapFs()
+	cmd := NewTransitionCmd(p, v, fs)
+	fileFlag := cmd.PersistentFlags().Lookup("file")
+	matchingGroupsFlag := cmd.PersistentFlags().Lookup("matching_groups")
+	ignorePatternsFlag := cmd.PersistentFlags().Lookup("ignore_patterns")
+	timeFormatFlag := cmd.PersistentFlags().Lookup("time_format")
+	formatFlag := cmd.PersistentFlags().Lookup("format")
+
+	assert.True(t, cmd.HasAvailablePersistentFlags(), "transition command should have available flag")
+	assert.NotNil(t, fileFlag, "transition command should have \"file\" flag")
+	assert.Equal(t, "f", fileFlag.Shorthand, "\"file\" flag's shorthand is \"f\"")
+	assert.Equal(t, "string", fileFlag.Value.Type(), "\"file\" flag is string")
+	assert.NotNil(t, matchingGroupsFlag, "transition command should have \"matching_groups\" flag")
+	assert.Equal(t, "m", matchingGroupsFlag.Shorthand, "\"matching_groups\" flag's shorthand is \"m\"")
+	assert.Equal(t, "stringSlice", matchingGroupsFlag.Value.Type(), "\"matching_groups\" flag is string slice")
+	assert.NotNil(t, ignorePatternsFlag, "transition command should have \"ignore_patterns\" flag")
+	assert.Equal(t, "stringSlice", ignorePatternsFlag.Value.Type(), "\"ignore_patterns\" flag is string slice")
+	assert.NotNil(t, timeFormatFlag, "transition command should have \"time_format\" flag")
+	assert.Equal(t, "string", timeFormatFlag.Value.Type(), "\"time_format\" flag is string")
+	assert.NotNil(t, formatFlag, "transition command should have \"format\" flag")
+	assert.Equal(t, "string", formatFlag.Value.Type(), "\"format\" flag is string")
+}
+
+func Test_TransitionCmd_RunE(t *testing.T) {
+	p := stool.NewTransitionProfiler()
+	v := viper.GetViper()
+	fs := afero.NewMemMapFs()
+	cmd := NewTransitionCmd(p, v, fs)
+
+	fileName := "./access.log"
+	v.Set("file", fileName)
+	_, _ = fs.Create(fileName)
+	_ = afero.WriteFile(fs, fileName, []byte("time:20/Jan/2023:14:39:01 +0900\thost:192.168.0.10\tforwardedfor:-\treq:POST /initialize HTTP/2.0\tstatus:200\tmethod:POST\turi:/initialize\tsize:18\treferer:-\tua:benchmarker-initializer\treqtime:0.268\tcache:-\truntime:-\tapptime:0.268\tvhost:192.168.0.11\tuidset:uid=0B00A8C0F528CA635B26685F02030303\tuidgot:-\tcookie:-\ntime:20/Jan/2023:14:39:06 +0900\thost:192.168.0.10\tforwardedfor:-\treq:GET / HTTP/2.0\tstatus:200\tmethod:GET\turi:/\tsize:528\treferer:-\tua:Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36 Edg/85.0.564.44\treqtime:0.002\tcache:-\truntime:-\tapptime:0.000\tvhost:192.168.0.11\tuidset:uid=0B00A8C0FA28CA635B26685F02040303\tuidgot:-\tcookie:-"), 0777)
+
+	stdout := new(bytes.Buffer)
+	cmd.SetOut(stdout)
+
+	err := cmd.RunE(cmd, []string{})
+
+	assert.NoError(t, err)
+	assert.Equal(t, "", stdout.String())
+}
+
+func Test_TransitionCmd_RunE_file_not_exists(t *testing.T) {
+	p := stool.NewTransitionProfiler()
+	v := viper.GetViper()
+	fs := afero.NewMemMapFs()
+	cmd := NewTransitionCmd(p, v, fs)
+
+	fileName := "./not_exists.log"
+	v.Set("file", fileName)
+
+	err := cmd.RunE(cmd, []string{})
+
+	assert.ErrorContains(t, err, "not_exists.log")
+}
+
+func Test_TransitionCmd_RunE_format_png(t *testing.T) {
+	p := stool.NewTransitionProfiler()
+	v := viper.GetViper()
+	fs := afero.NewMemMapFs()
+	cmd := NewTransitionCmd(p, v, fs)
+
+	fileName := "./access.log"
+	v.Set("file", fileName)
+	v.Set("format", "png")
+	_, _ = fs.Create(fileName)
+	_ = afero.WriteFile(fs, fileName, []byte("time:20/Jan/2023:14:39:01 +0900\thost:192.168.0.10\tforwardedfor:-\treq:POST /initialize HTTP/2.0\tstatus:200\tmethod:POST\turi:/initialize\tsize:18\treferer:-\tua:benchmarker-initializer\treqtime:0.268\tcache:-\truntime:-\tapptime:0.268\tvhost:192.168.0.11\tuidset:uid=0B00A8C0F528CA635B26685F02030303\tuidgot:-\tcookie:-\ntime:20/Jan/2023:14:39:06 +0900\thost:192.168.0.10\tforwardedfor:-\treq:GET / HTTP/2.0\tstatus:200\tmethod:GET\turi:/\tsize:528\treferer:-\tua:Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36 Edg/85.0.564.44\treqtime:0.002\tcache:-\truntime:-\tapptime:0.000\tvhost:192.168.0.11\tuidset:uid=0B00A8C0FA28CA635B26685F02040303\tuidgot:-\tcookie:-"), 0777)
+
+	stdout := new(bytes.Buffer)
+	cmd.SetOut(stdout)
+
+	err := cmd.RunE(cmd, []string{})
+
+	assert.NoError(t, err)
+	assert.Equal(t, "", stdout.String())
+}
+
+func Test_TransitionCmd_RunE_format_csv(t *testing.T) {
+	p := stool.NewTransitionProfiler()
+	v := viper.GetViper()
+	fs := afero.NewMemMapFs()
+	cmd := NewTransitionCmd(p, v, fs)
+
+	fileName := "./access.log"
+	v.Set("file", fileName)
+	v.Set("format", "csv")
+	_, _ = fs.Create(fileName)
+	_ = afero.WriteFile(fs, fileName, []byte("time:20/Jan/2023:14:39:01 +0900\thost:192.168.0.10\tforwardedfor:-\treq:POST /initialize HTTP/2.0\tstatus:200\tmethod:POST\turi:/initialize\tsize:18\treferer:-\tua:benchmarker-initializer\treqtime:0.268\tcache:-\truntime:-\tapptime:0.268\tvhost:192.168.0.11\tuidset:uid=0B00A8C0F528CA635B26685F02030303\tuidgot:-\tcookie:-\ntime:20/Jan/2023:14:39:06 +0900\thost:192.168.0.10\tforwardedfor:-\treq:GET / HTTP/2.0\tstatus:200\tmethod:GET\turi:/\tsize:528\treferer:-\tua:Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36 Edg/85.0.564.44\treqtime:0.002\tcache:-\truntime:-\tapptime:0.000\tvhost:192.168.0.11\tuidset:uid=0B00A8C0FA28CA635B26685F02040303\tuidgot:-\tcookie:-"), 0777)
+
+	stdout := new(bytes.Buffer)
+	cmd.SetOut(stdout)
+
+	err := cmd.RunE(cmd, []string{})
+
+	assert.NoError(t, err)
+	assert.Equal(t, ",,GET /,POST /initialize\n,0,0,1\nGET /,1,0,0\nPOST /initialize,0,1,0\n", stdout.String())
 }
 
 func Test_logNorm(t *testing.T) {
