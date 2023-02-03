869e595e95eddb1ff3807861396e4360766c1763
haijima <k.haijima@gmail.com>
2023-02-03 10:33:46 +0900

    fix: Fix to show version when built in GitHub Actions

--- cmd/version.go
+++ cmd/version.go
@@ -8,36 +8,27 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Version is set in build step
+var Version = "unset"
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the version of stool command",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Fprintf(cmd.OutOrStdout(), `Version: %s
-Revision: %s
 OS: %s
 Arch: %s
-`, version(), revision(), runtime.GOOS, runtime.GOARCH)
+`, version(), runtime.GOOS, runtime.GOARCH)
 	},
 }
 
 func version() string {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return "(devel)"
+	if Version != "" {
+		return Version
 	}
-	return info.Main.Version
-}
-
-func revision() string {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return "(devel)"
-	}
-	for _, setting := range info.Settings {
-		if setting.Key == "vcs.revision" {
-			return setting.Value
-		}
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		return buildInfo.Main.Version
 	}
 	return "(devel)"
 }
