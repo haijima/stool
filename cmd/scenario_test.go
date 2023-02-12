4900e3d886777b2665d038a579a2c25c4c3ce208
haijima <k.haijima@gmail.com>
2023-02-12 22:26:06 +0900

    test: Fix benchmarks to use a log file

--- cmd/scenario_test.go
+++ cmd/scenario_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/haijima/stool/internal"
@@ -9,17 +10,16 @@ import (
 	"github.com/spf13/viper"
 )
 
-func BenchmarkNewScenarioCommand_Trend_RunE(b *testing.B) {
+func BenchmarkScenarioCommand_RunE(b *testing.B) {
 	p := internal.NewScenarioProfiler()
 	v := viper.New()
-	fs := afero.NewMemMapFs()
+	fs := afero.NewOsFs()
 	cmd := NewScenarioCmd(p, v, fs)
 
-	fileName := "./access.log"
+	dir, _ := os.Getwd()
+	fileName := dir + "/testdata/access.log"
 	v.Set("file", fileName)
 	v.Set("interval", "5")
-	_, _ = fs.Create(fileName)
-	_ = afero.WriteFile(fs, fileName, []byte("time:20/Jan/2023:14:39:01 +0900\thost:192.168.0.10\tforwardedfor:-\treq:POST /initialize HTTP/2.0\tstatus:200\tmethod:POST\turi:/initialize\tsize:18\treferer:-\tua:benchmarker-initializer\treqtime:0.268\tcache:-\truntime:-\tapptime:0.268\tvhost:192.168.0.11\tuidset:uid=0B00A8C0F528CA635B26685F02030303\tuidgot:-\tcookie:-\ntime:20/Jan/2023:14:39:06 +0900\thost:192.168.0.10\tforwardedfor:-\treq:GET / HTTP/2.0\tstatus:200\tmethod:GET\turi:/\tsize:528\treferer:-\tua:Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36 Edg/85.0.564.44\treqtime:0.002\tcache:-\truntime:-\tapptime:0.000\tvhost:192.168.0.11\tuidset:uid=0B00A8C0FA28CA635B26685F02040303\tuidgot:-\tcookie:-"), 0777)
 
 	stdout := new(bytes.Buffer)
 	cmd.SetOut(stdout)
