1f281a0b09bf21d453f4e11e178d453916927f8a
HAIJIMA Kazuki <haijima@users.noreply.github.com>
2023-02-16 00:09:32 +0900

    refactor: Refactor graphviz (#23)

--- internal/graphviz/graph.go
+++ internal/graphviz/graph.go
@@ -1,34 +1,56 @@
 package graphviz
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/awalterschulze/gographviz"
 )
 
-func NewEscapedDirectedGraph(title string, isHorizontal bool) (*gographviz.Escape, error) {
+type Graph struct {
+	name         string
+	title        string
+	IsHorizontal bool
+	parent       *Graph
+	g            *gographviz.Escape
+}
+
+func NewGraph(name string, title string) *Graph {
+	return &Graph{name: name, title: title}
+}
+
+func (g *Graph) toRootGraphviz() (*gographviz.Escape, error) {
+	if g.parent != nil {
+		return g.parent.toRootGraphviz()
+	}
+	if g.g != nil {
+		return g.g, nil
+	}
+
 	graph := gographviz.NewEscape()
-	if err := graph.SetName("root"); err != nil {
+	if err := graph.SetName(g.name); err != nil {
 		return nil, err
 	}
-	if err := graph.AddAttr("root", "label", title); err != nil {
+	if err := graph.AddAttr(g.name, "label", g.title); err != nil {
 		return nil, err
 	}
-	if err := graph.AddAttr("root", "tooltip", title); err != nil {
+	if err := graph.AddAttr(g.name, "tooltip", g.title); err != nil {
 		return nil, err
 	}
-	if err := graph.AddAttr("root", "labelloc", "t"); err != nil {
+	if err := graph.AddAttr(g.name, "labelloc", "t"); err != nil {
 		return nil, err
 	}
-	if err := graph.AddAttr("root", "fontname", "Courier"); err != nil {
+	if err := graph.AddAttr(g.name, "fontname", "Courier"); err != nil {
 		return nil, err
 	}
-	if err := graph.AddAttr("root", "margin", "20"); err != nil {
+	if err := graph.AddAttr(g.name, "margin", "20"); err != nil {
 		return nil, err
 	}
 	if err := graph.SetDir(true); err != nil {
 		return nil, err
 	}
 
-	if isHorizontal {
+	if g.IsHorizontal {
 		if err := graph.AddAttr("root", "labeljust", "l"); err != nil {
 			return nil, err
 		}
@@ -36,19 +58,93 @@ func NewEscapedDirectedGraph(title string, isHorizontal bool) (*gographviz.Escap
 			return nil, err
 		}
 	}
+	g.g = graph
 
 	return graph, nil
 }
 
-func AddSubgraph(parent *gographviz.Escape, subGraphName string, subGraphTitle string) error {
-	return parent.AddSubGraph(parent.Name, subGraphName, map[string]string{
-		"label":     subGraphTitle,
-		"tooltip":   subGraphTitle,
+func (g *Graph) AddSubGraph(subGraph *Graph) error {
+	graph, err := g.toRootGraphviz()
+	if err != nil {
+		return err
+	}
+
+	if err := graph.AddSubGraph(g.name, subGraph.name, map[string]string{
+		"label":     subGraph.title,
+		"tooltip":   subGraph.title,
 		"style":     "filled",
 		"labelloc":  "t",
 		"labeljust": "l",
 		"color":     "#cccccc",
 		"fillcolor": "#ffffff",
 		"sep":       "20",
+	}); err != nil {
+		return err
+	}
+
+	subGraph.parent = g
+	return nil
+}
+
+func (g *Graph) AddBoxNode(node *BoxNode) error {
+	graph, err := g.toRootGraphviz()
+	if err != nil {
+		return err
+	}
+
+	return graph.AddNode(g.name, node.Name, map[string]string{
+		"shape":     "box",
+		"style":     "filled",
+		"fontname":  "Courier",
+		"penwidth":  "1",
+		"margin":    "0.2",
+		"color":     node.Color,
+		"fillcolor": node.FillColor,
+		"fontsize":  fmt.Sprintf("%f", node.FontSize),
+		"label":     node.Title,
+		"tooltip":   node.Title,
 	})
+}
+
+func (g *Graph) AddTextNode(node *TextNode) error {
+	graph, err := g.toRootGraphviz()
+	if err != nil {
+		return err
+	}
+
+	return graph.AddNode(g.name, node.Name, map[string]string{
+		"shape":    "plaintext",
+		"fontname": "Courier",
+		"fontsize": fmt.Sprintf("%f", node.FontSize),
+		"label":    node.Title,
+		"tooltip":  node.Title,
+	})
+}
+
+func (g *Graph) AddEdge(edge *Edge) error {
+	graph, err := g.toRootGraphviz()
+	if err != nil {
+		return err
+	}
+
+	dir := "forward"
+	if edge.fromName == edge.toName {
+		dir = "back"
+	}
+
+	return graph.AddEdge(edge.fromName, edge.toName, true, map[string]string{
+		"color":    edge.Color,
+		"penwidth": fmt.Sprintf("%f", edge.PenWidth),
+		"weight":   fmt.Sprintf("%f", edge.Weight),
+		"dir":      dir,
+	})
+}
+
+func (g *Graph) Write(w io.Writer) error {
+	graph, err := g.toRootGraphviz()
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, graph.String())
+	return err
 }
