42dd292ce6a6053d3580f240254e5d526a8dfeaa
HAIJIMA Kazuki <haijima@users.noreply.github.com>
2023-04-04 00:22:06 +0900

    feat: Colorize trend output (#36)

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"os"
 
 	"github.com/haijima/stool/cmd"
+	"github.com/mattn/go-colorable"
 	"github.com/spf13/afero"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	rootCmd := cmd.NewRootCmd(viper.New(), afero.NewOsFs())
+	rootCmd.SetErr(colorable.NewColorableStderr())
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
